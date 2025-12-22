93bd7f46e29d0191fd1e595b9cae9f1176e7883e
Taisuke Fukuno <fukuno@jig.jp>
2025-12-22 17:49:51 +0900

    fix param name

--- OpenEXR.js
+++ OpenEXR.js
@@ -2,8 +2,8 @@ import { encodeEXR_RGBA16F } from "./encodeEXR16.js";
 import { decodeEXR_RGBA16F } from "./decodeEXR16.js";
 
 export class OpenEXR {
-  static encode(imagedataf32) {
-    return encodeEXR_RGBA16F(imagedataf32.width, imagedataf32.height, imagedataf32.data);
+  static encode(imagedataf16) {
+    return encodeEXR_RGBA16F(imagedataf16.width, imagedataf16.height, imagedataf16.data);
   }
   static decode(bin) {
     return decodeEXR_RGBA16F(bin);
